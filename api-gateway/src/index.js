7fcbec41970ce4428487d832cba2a4b405b7580f
aeecheverry <aeecheverry@uninorte.edu.co>
2019-06-14 15:17:48 -0500

    Api-gateway added to branch gateway

---
+++ api-gateway/src/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+
+app.post("/login", async (req, res)=>{
+    
+});
+
+app.post("/register", async (req, res)=>{
+    
+});
+
+const port = 8080;
+app.listen(port,()=>{
+    console.log("Server up and ready!")
+});
